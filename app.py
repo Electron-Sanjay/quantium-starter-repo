fef72cc8f5bf37a5573d56e7045dc699e54591a7
electron_sanjay <sanjaybhargava1501@gmail.com>
2024-02-15 11:31:59 -0500

    added the radio buttons

--- app.py
+++ app.py
@@ -1,30 +1,97 @@
-from dash import Dash, html,dcc
-import plotly.express as px
 import pandas as pd
+from dash import Dash, html, dcc, Input, Output
 
-app = Dash(__name__)
+from plotly.express import line
+
+# the path to the formatted data file
+COLORS = {
+    "primary": "#FEDBFF",
+    "secondary": "#D598EB",
+    "font": "#522A61"
+}
+
+# load in data
 data = pd.read_csv("D:/Forage/Quantium/sw/quantium-starter-repo/final.csv")
+data = data.sort_values(by="date")
 
-data = data.sort_values(by= "date", ascending= True)
+# initialize dash
+dash_app = Dash(__name__)
 
 
-fig = px.line(data,x= "date",y= "Total Sales in $")
-
-app.layout = html.Div(children=[
-    html.H1(children='Sales of Pink Morsel'),
-
-    html.Div(children='''
-        Below graph shows the sales of the Pink Morsel.
-    '''),
-
-    dcc.Graph(
-        id='line-graph',
-        figure=fig
+# create the visualization
+def generate_figure(chart_data):
+    line_chart = line(chart_data, x="date", y="Total Sales in $", title="Pink Morsel Sales")
+    line_chart.update_layout(
+        plot_bgcolor=COLORS["secondary"],
+        paper_bgcolor=COLORS["primary"],
+        font_color=COLORS["font"]
     )
-])
+    return line_chart
 
+
+visualization = dcc.Graph(
+    id="visualization",
+    figure=generate_figure(data)
+)
+
+# create the header
+header = html.H1(
+    "Pink Morsel Visualizer",
+    id="header",
+    style={
+        "background-color": COLORS["secondary"],
+        "color": COLORS["font"],
+        "border-radius": "20px"
+    }
+)
+
+# region picker
+region_picker = dcc.RadioItems(
+    ["north", "east", "south", "west", "all"],
+    "north",
+    id="region_picker",
+    inline=True
+)
+region_picker_wrapper = html.Div(
+    [
+        region_picker
+    ],
+    style={
+        "font-size": "150%"
+    }
+)
+
+
+# define the region picker callback
+@dash_app.callback(
+    Output(visualization, "figure"),
+    Input(region_picker, "value")
+)
+def update_graph(region):
+    # filter the dataset
+    if region == "all":
+        trimmed_data = data
+    else:
+        trimmed_data = data[data["region"] == region]
+
+    # generate a new line chart with the filtered data
+    figure = generate_figure(trimmed_data)
+    return figure
+
+# define the app layout
+dash_app.layout = html.Div(
+    [
+        header,
+        visualization,
+        region_picker_wrapper
+    ],
+    style={
+        "textAlign": "center",
+        "background-color": COLORS["primary"],
+        "border-radius": "20px"
+    }
+)
+
+# this is only true if the module is executed as the program entrypoint
 if __name__ == '__main__':
-    app.run(debug=True)
-
-"""From the dash we can say that the sales are increased even 
-after the price of the Pink Morsel increase"""
+    dash_app.run_server()
